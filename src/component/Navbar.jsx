3066addbeaaea603fc8f8ba735cd1de4b6ffb1e5
Mahmoud Rabea Assaf <bodamomo2010@gmail.com>
2025-10-04 19:29:56 +0300

    igrp oj

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,27 +1,23 @@
 import React from 'react';
 import { PlusIcon } from "lucide-react";
-import { Link } from 'react-router'; 
+import { Link } from 'react-router-dom'; 
 
 export default function Navbar() {
   return (
-    <div>
-      <header>
-        <div className='bg-base-300 w-full p-4'> 
-          <div className='mx-auto max-w-4xl'> 
-            <div className='flex items-center justify-between'> 
-              <h1 className='text-xl text-red-50'>Jot </h1> 
-              <div>
-                <Link 
-                 to='/create' 
-                 className='btn btn-primary flex items-center gap-2 rounded-xl px-5 shadow-md hover:scale-105 transition-transform text-white'>
-                  <PlusIcon className="size-5" /> 
-                  <span>New Note</span>
-                  </Link>
-              </div>
-            </div>
+    <header>
+      <div className='bg-gray-700 w-full p-4'> 
+        <div className='mx-auto max-w-4xl'> 
+          <div className='flex items-center justify-between'> 
+            <h1 className='text-xl text-white font-bold'>JotNote</h1> 
+            <Link 
+              to='/create' 
+              className='btn btn-primary flex items-center gap-2 rounded-xl px-5 shadow-md hover:scale-105 transition-transform text-white'>
+              <PlusIcon className="size-5" /> 
+              <span>New Note</span>
+            </Link>
           </div>
         </div>
-      </header>
-    </div>
-  )
+      </div>
+    </header>
+  );
 }
